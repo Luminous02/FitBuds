f5616cf039e32410dba38a2258562e1a7a538edb
miaglenn <miaxiaoqiao11@gmail.com>
2025-02-27 16:51:50 -0500

    Was able to redirect success login to dashboard page

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
-import { useState } from "react";
+import { BrowserRouter as Router, Routes, Route, useNavigate } from "react-router-dom";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import LoginForm from "./LoginForm/LoginForm";
+import Dashboard from "./Dashboard/dash";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <LoginForm />
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<LoginForm />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </Router>
   );
 }
 
--- src/LoginForm/LoginForm.jsx
+++ src/LoginForm/LoginForm.jsx
@@ -1,15 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import "./LoginForm.css";
 import { FaUser, FaLock } from "react-icons/fa";
-import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const LoginForm = () => {
 
   const [user, setUser] = useState({ username: '', password: '' })
+  const navigate = useNavigate();
+
   const handleLogin = async (e) => {
     e.preventDefault();
     if(user.username === "guest" && user.password === "password"){
       alert("Valid user");
+      navigate("/dashboard");
     } else{
       alert("Invalid user");
     }
@@ -46,36 +49,6 @@ const LoginForm = () => {
         <div className="register-link">
           <p>
             Don't have an account?<a href="#"> Register</a>
-          </p>
-        </div>
-      </form>
-
-      <form action="">
-        <h1>Register</h1>
-
-        <div className="input-box">
-          <input type="text" placeholder="Username" required 
-          value={user.username}
-          onChange={(e) => setUser({ ...user, username: e.target.value })}/>
-          <FaUser className="icon" />
-        </div>
-        <div className="input-box">
-          <input type="password" placeholder="Password" required 
-          value={user.password}
-          onChange={(e) => setUser({ ...user, password: e.target.value })}/>
-          <FaLock className="icon" />
-        </div>
-        <div className="input-box">
-          <input type="password" placeholder="Password" required 
-          value={user.password}
-          onChange={(e) => setUser({ ...user, password: e.target.value })}/>
-          <FaLock className="icon" />
-        </div>
-
-        <button type="submit" onClick={(e) => handleLogin(e)}>Register</button>
-        <div className="login-link">
-          <p>
-            Already have an account?<a href="#"> Login</a>
           </p>
         </div>
       </form>
