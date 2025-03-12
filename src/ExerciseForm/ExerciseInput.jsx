ec16652eac667bed09e4fc2e4e3df2df989d9ee9
Branine <jbeall1@highpoint.edu>
2025-03-12 14:19:30 -0400

    fixed checkpoint

--- src/ExerciseForm/ExerciseInput.jsx
+++ src/ExerciseForm/ExerciseInput.jsx
@@ -24,7 +24,7 @@ const ExerciseInput = ({ onAddExercise }) => {
     };
 
     const handleViewExercises = () => {
-        navigate("/exercise-output");
+        navigate("/dashboard/exercise-output");
     };
 
     return (
