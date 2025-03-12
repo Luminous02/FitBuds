26afab085ba1b2ab2b69ee6a35dd2343c1f6d78f
Will <91991926+Luminous02@users.noreply.github.com>
2025-03-12 13:36:20 -0400

    Bring in updated homepage and styling (#4)
    * Day of Meeting Changes
    
    * Update colors on dash

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,7 +1,60 @@
 import React from "react";
+import "./Home.css";
 
 const Home = () => {
-    return <h1>Home Page</h1>
+  return (
+    <div className="container">
+      <div className="feedScroll">
+        <h1>Feed</h1>
+        <div className="feedCard">
+          <h3>Full Name</h3>
+          <h4>@username</h4>
+          <p>
+            <h4>34:05</h4> Minute Run
+          </p>
+          <button>Like</button>
+          <button>Share</button>
+          <button id="moreButton">More</button>
+        </div>
+
+        <div className="feedCard">
+          <h3>Full Name</h3>
+          <h4>@username</h4>
+          <p>
+            <h4>34:05</h4> Minute Run
+          </p>
+          <button>Like</button>
+          <button>Share</button>
+          <button id="moreButton">More</button>
+        </div>
+
+        <div className="feedCard">
+          <h3>Full Name</h3>
+          <h4>@username</h4>
+          <p>
+            <h4>34:05</h4> Minute Run
+          </p>
+          <button>Like</button>
+          <button>Share</button>
+          <button id="moreButton">More</button>
+        </div>
+
+        <div className="feedCard">
+          <h3>Full Name</h3>
+          <h4>@username</h4>
+          <p>
+            <h4>34:05</h4> Minute Run
+          </p>
+          <button>Like</button>
+          <button>Share</button>
+          <button id="moreButton">More</button>
+        </div>
+      </div>
+      <div className="leaderScroll">
+        <h1>Leaderboard</h1>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
