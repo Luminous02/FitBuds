82f83e28412c922d2753d94f22c53fbeead682b8
Luminous02 <91991926+Luminous02@users.noreply.github.com>
2025-03-13 18:16:39 -0400

    Improved Feed on Dashboard

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,58 +1,121 @@
 import React from "react";
 import "./Home.css";
+import { FiThumbsUp, FiMoreHorizontal, FiShare } from "react-icons/fi";
 
 const Home = () => {
   return (
     <div className="homeContainer">
+      <h1>Feed</h1>
+      <div className="gradientBlock"></div>
       <div className="feedScroll">
-        <h1>Feed</h1>
         <div className="feedCard">
-          <h3>Full Name</h3>
-          <h4>@username</h4>
-          <p>
-            <h4>34:05</h4> Minute Run
-          </p>
-          <button>Like</button>
-          <button>Share</button>
-          <button id="moreButton">More</button>
-        </div>
+          <div className="cardUserData">
+            <h3>Full Name</h3>
+            <h5>@username</h5>
+          </div>
 
-        <div className="feedCard">
-          <h3>Full Name</h3>
-          <h4>@username</h4>
-          <p>
-            <h4>34:05</h4> Minute Run
-          </p>
-          <button>Like</button>
-          <button>Share</button>
-          <button id="moreButton">More</button>
-        </div>
+          <div className="exerciseData">
+            <h4>34:05</h4>
+            <p>Minute Run</p>
+          </div>
 
-        <div className="feedCard">
-          <h3>Full Name</h3>
-          <h4>@username</h4>
-          <p>
-            <h4>34:05</h4> Minute Run
-          </p>
-          <button>Like</button>
-          <button>Share</button>
-          <button id="moreButton">More</button>
+          <button id="likeButton">
+            <FiThumbsUp />
+          </button>
+          <button id="shareButton">
+            <FiShare />
+          </button>
+          <button id="moreButton">
+            <FiMoreHorizontal />
+          </button>
         </div>
-
         <div className="feedCard">
-          <h3>Full Name</h3>
-          <h4>@username</h4>
-          <p>
-            <h4>34:05</h4> Minute Run
-          </p>
-          <button>Like</button>
-          <button>Share</button>
-          <button id="moreButton">More</button>
+          <div className="cardUserData">
+            <h3>Full Name</h3>
+            <h5>@username</h5>
+          </div>
+
+          <div className="exerciseData">
+            <h4>34:05</h4>
+            <p>Minute Run</p>
+          </div>
+
+          <button id="likeButton">
+            <FiThumbsUp />
+          </button>
+          <button id="shareButton">
+            <FiShare />
+          </button>
+          <button id="moreButton">
+            <FiMoreHorizontal />
+          </button>
+        </div>
+        <div className="feedCard">
+          <div className="cardUserData">
+            <h3>Full Name</h3>
+            <h5>@username</h5>
+          </div>
+
+          <div className="exerciseData">
+            <h4>34:05</h4>
+            <p>Minute Run</p>
+          </div>
+
+          <button id="likeButton">
+            <FiThumbsUp />
+          </button>
+          <button id="shareButton">
+            <FiShare />
+          </button>
+          <button id="moreButton">
+            <FiMoreHorizontal />
+          </button>
+        </div>
+        <div className="feedCard">
+          <div className="cardUserData">
+            <h3>Full Name</h3>
+            <h5>@username</h5>
+          </div>
+
+          <div className="exerciseData">
+            <h4>34:05</h4>
+            <p>Minute Run</p>
+          </div>
+
+          <button id="likeButton">
+            <FiThumbsUp />
+          </button>
+          <button id="shareButton">
+            <FiShare />
+          </button>
+          <button id="moreButton">
+            <FiMoreHorizontal />
+          </button>
+        </div>
+        <div className="feedCard">
+          <div className="cardUserData">
+            <h3>Full Name</h3>
+            <h5>@username</h5>
+          </div>
+
+          <div className="exerciseData">
+            <h4>34:05</h4>
+            <p>Minute Run</p>
+          </div>
+
+          <button id="likeButton">
+            <FiThumbsUp />
+          </button>
+          <button id="shareButton">
+            <FiShare />
+          </button>
+          <button id="moreButton">
+            <FiMoreHorizontal />
+          </button>
         </div>
       </div>
-      <div className="leaderScroll">
-        <h1>Leaderboard</h1>
-      </div>
+      <h1>Leaderboard</h1>
+      <div className="leaderScroll"></div>
     </div>
   );
 };
