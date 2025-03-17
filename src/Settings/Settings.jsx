aa9377bfef7566e46573c008ff974893c9092c9c
Branine <jbeall1@highpoint.edu>
2025-03-17 14:30:30 -0400

    stafe

--- src/Settings/Settings.jsx
+++ src/Settings/Settings.jsx
@@ -1,7 +1,31 @@
-import React from "react";
+import React, {useState} from "react";
+import "./Settings.css";
 
 const Settings = () => {
-    return <h1>Settings Page</h1>
+    const [settings, setSettings] = useState({
+        name: "John Doe",
+        email: "johndoe@example.com",
+        password: "",
+        unitTime: "minutes",
+        unitWeight: "lbs",
+        difficulty: "medium",
+        notifications: true,
+        privateProfile: false,
+
+    });
+
+    const handleChange = (e) => {
+        const { name, type, checked, value } = e.target;
+        setSettings({
+            ...settings,
+            [name]: type === "checkbox" ? checked : value,
+        });
+    };
+
+
+    return (
+        <h1>Settings Page</h1>
+    );
 };
 
 export default Settings;
