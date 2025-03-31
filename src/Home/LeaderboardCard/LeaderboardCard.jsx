2e54ccd2f370ab0f6fdcac2aed26b9366d4ecde4
miaglenn <miaxiaoqiao11@gmail.com>
2025-03-31 09:48:14 -0400

    Editing leaderboard and added profile pics

--- src/Home/LeaderboardCard/LeaderboardCard.jsx
+++ src/Home/LeaderboardCard/LeaderboardCard.jsx
@@ -3,7 +3,17 @@ import "./LeaderboardCard.css";
 const LeaderboardCard = () => {
   return (
     <div className="leaderboardCard">
-      <h1>hi</h1>
+      <div className="lboard_section">
+        <div className="lboard_tabs">
+          <div className="tabs">
+            <ul>
+              <li className="active" data-li="today">Today</li>
+              <li data-li="week">Week</li>
+              <li data-li="month">Month</li>
+            </ul>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
